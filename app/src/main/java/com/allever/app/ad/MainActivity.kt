32cd6b79684a88c4b131f9ba979df08dfe61323b
devallever <devallever@163.com>
2019-11-08 14:51:16 +0800

    init

---
+++ app/src/main/java/com/allever/app/ad/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.allever.app.ad
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
