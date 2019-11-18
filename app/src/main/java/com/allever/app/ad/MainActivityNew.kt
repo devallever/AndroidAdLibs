0a0ac92378d03e1cf49d24834e536436c3447c12
devallever <devallever@163.com>
2019-11-18 19:02:44 +0800

    内容：添加AdMob库

---
+++ app/src/main/java/com/allever/app/ad/MainActivityNew.kt
@@ -0,0 +1,11 @@
+package com.allever.app.ad
+
+import android.os.Bundle
+import com.allever.lib.common.app.BaseActivity
+
+class MainActivityNew: BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
