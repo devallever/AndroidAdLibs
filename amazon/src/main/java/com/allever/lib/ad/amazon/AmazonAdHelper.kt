dba24195e1e3a1aefc23e4cba924ce0f6c8b36f8
devallever <devallever@163.com>
2019-11-20 15:07:26 +0800

    内容：添加亚马逊模块

---
+++ amazon/src/main/java/com/allever/lib/ad/amazon/AmazonAdHelper.kt
@@ -0,0 +1,56 @@
+package com.allever.lib.ad.amazon
+
+import android.content.Context
+import com.allever.lib.ad.ADType
+import com.allever.lib.ad.AdManager
+import com.allever.lib.ad.BaseAd
+import com.allever.lib.common.util.loge
+import com.amazon.device.ads.AdRegistration
+
+object AmazonAdHelper: AdManager {
+    /***
+     * appId 就是appkey
+     */
+    override fun init(context: Context, appId: String, appKey: String, appToken: String) {
+        // For debugging purposes enable logging, but disable for production builds.
+        AdRegistration.enableLogging(BuildConfig.DEBUG)
+        // For debugging purposes flag all ad requests as tests, but set to false for production builds.
+        AdRegistration.enableTesting(BuildConfig.DEBUG)
+        try {
+            AdRegistration.setAppKey(appId)
+        } catch (e: IllegalArgumentException) {
+            loge("IllegalArgumentException thrown: $e")
+            return
+        }
+    }
+
+    override fun createAd(adType: ADType): BaseAd? {
+        return when(adType) {
+            ADType.BANNER -> {
+                return createBannerAd()
+            }
+            ADType.INSERT -> {
+                return createInsertAd()
+            }
+            ADType.VIDEO -> {
+                return createVideoAd()
+            }
+            ADType.DOWNLOAD -> {
+                return createDownloadAd()
+            }
+
+            else -> null
+        }
+    }
+
+    override fun createBannerAd(): BaseAd? = AmazonBanner()
+
+    override fun createInsertAd(): BaseAd? = AmazonInsert()
+
+    override fun createVideoAd(): BaseAd? = AmazonVideo()
+
+    override fun createDownloadAd(): BaseAd? = null
+
+    override fun destroy(context: Context) {
+    }
+}
---
+++ amazon/src/main/java/com/allever/lib/ad/amazon/AmazonVideo.kt
@@ -0,0 +1,21 @@
+package com.allever.lib.ad.amazon
+
+import android.view.ViewGroup
+import com.allever.lib.ad.AdListener
+import com.allever.lib.ad.BaseAd
+
+class AmazonVideo : BaseAd(){
+    override fun load(adPosition: String, container: ViewGroup?, adListener: AdListener?) {
+        adListener?.onFailed()
+    }
+
+    override fun show() {
+    }
+
+    override fun loadAndShow(adPosition: String, container: ViewGroup?, adListener: AdListener?) {
+        adListener?.onFailed()
+    }
+
+    override fun destroy() {
+    }
+}
