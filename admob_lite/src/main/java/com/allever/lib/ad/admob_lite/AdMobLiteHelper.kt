863a9e24dae64ea96dfbb8f9355b9112356ba70b
devallever <devallever@163.com>
2019-11-21 14:17:13 +0800

    内容：降低admobLite版本

--- admob_lite/src/main/java/com/allever/lib/ad/admob_lite/AdMobLiteHelper.kt
+++ admob_lite/src/main/java/com/allever/lib/ad/admob_lite/AdMobLiteHelper.kt
@@ -9,9 +9,9 @@ import com.google.android.gms.ads.MobileAds
 object AdMobLiteHelper: AdManager {
     var testDevicesList = mutableListOf<String>()
     override fun init(context: Context, appId: String, appKey: String, appToken: String) {
-        MobileAds.initialize(context) {
-
-        }
+//        MobileAds.initialize(context) {
+//
+//        }
         testDevicesList.add("811A5A5DA1BF1E2FC9EE39041EC322FF")
         testDevicesList.add("1621DB3C172AE6711BA840F4AEF6EF48")
     }
