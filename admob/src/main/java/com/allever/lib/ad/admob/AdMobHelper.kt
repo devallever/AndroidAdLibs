9cb57351b80ab3da7c038257709ccd7ec3d1e140
devallever <devallever@163.com>
2019-11-21 15:24:17 +0800

    内容：添加视频广告

--- admob/src/main/java/com/allever/lib/ad/admob/AdMobHelper.kt
+++ admob/src/main/java/com/allever/lib/ad/admob/AdMobHelper.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import com.allever.lib.ad.ADType
 import com.allever.lib.ad.AdManager
 import com.allever.lib.ad.BaseAd
+import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.MobileAds
 
 object AdMobHelper: AdManager {
@@ -12,10 +13,8 @@ object AdMobHelper: AdManager {
         MobileAds.initialize(context) {
 
         }
-        if (BuildConfig.DEBUG) {
-            testDevicesList.add("811A5A5DA1BF1E2FC9EE39041EC322FF")
-            testDevicesList.add("1621DB3C172AE6711BA840F4AEF6EF48")
-        }
+        testDevicesList.add("811A5A5DA1BF1E2FC9EE39041EC322FF")
+        testDevicesList.add("1621DB3C172AE6711BA840F4AEF6EF48")
     }
 
     override fun createAd(adType: ADType): BaseAd? {
@@ -45,10 +44,20 @@ object AdMobHelper: AdManager {
         return AdMobInsert()
     }
 
-    override fun createVideoAd(): BaseAd? = null
+    override fun createVideoAd(): BaseAd? = AdMobVideo()
 
     override fun createDownloadAd(): BaseAd? = null
 
     override fun destroy(context: Context) {
     }
+
+    fun createAdRequest(): AdRequest {
+        //加载请求
+        val reqBuild = AdRequest.Builder()
+        for (device in testDevicesList) {
+            reqBuild.addTestDevice(device)
+        }
+        return reqBuild.build()
+    }
+
 }
